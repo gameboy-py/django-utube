d411fb4ae362227682a9723d9642b2d0ee8ac0a5
gameboy-py <66083598+gameboy-py@users.noreply.github.com>
2020-06-21 10:44:07 +0530

    Update settings.py

--- DEMOPROJECT/settings.py
+++ DEMOPROJECT/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = "gameboytestmail@gmail.com"
+EMAIL_HOST_USER = ""
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_PASSWORD = ""
